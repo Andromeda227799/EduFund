7cae36d9952c5a498d4aca9bc805a35b6b48f5cd
Shamith Rao <shamith2709@gmail.com>
2021-11-09 16:22:30 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SafeScreen from './app/screens/SafeScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeScreen style={styles.container}>
+      <Text>App.js to start working on EDUFUNd!</Text>
+      
+    </SafeScreen>
   );
 }
 
---
+++ app/config/colors.js
@@ -0,0 +1,15 @@
+export default {
+    primary: "#00bfff",
+    //secondary: "#4ecdc4",
+    secondary: "#00d652",
+  
+    black: "#000",
+    white: "#fff",
+    medium: "#6e6969",
+    red: "#eb4934",
+    screen: "#f0f0f0",
+    yellow: "#b3af39",
+    lightGrey: "#858585",
+    lightBlue: "#abe9ff",
+  };
+  
